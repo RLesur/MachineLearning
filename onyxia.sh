a21a4469e70479f508e4977ac68a794bd6900e05
Romain Lesur <romain.lesur@gmail.com>
2020-04-21 16:55:06 +0200

    pass command to bash

--- onyxia.sh
+++ onyxia.sh
@@ -2,4 +2,4 @@ conda init bash
 git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
 cd MachineLearning
 conda env create -f environment.yml
-conda activate ml-environment
+bash -c "conda activate ml-environment"
