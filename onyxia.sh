2c943add8e7bfe760d362bad5ba718f13e5e17a1
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 06:39:13 +0200

    failed: comment on install step

--- onyxia.sh
+++ onyxia.sh
@@ -3,4 +3,4 @@ git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
 # Change directory
 cd MachineLearning
 # Install dependencies (from https://stackoverflow.com/a/38609653/6500804)
-while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
+#while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
