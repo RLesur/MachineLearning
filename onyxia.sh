6cfb963316f3422654f9977152fb9f6edf173d9b
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 06:43:28 +0200

    test with a void script

--- onyxia.sh
+++ onyxia.sh
@@ -1,5 +1,5 @@
 # Clone the repository
-git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
+# git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
 
 # Install dependencies
 # pip install -r MachineLearning/requirements.txt
