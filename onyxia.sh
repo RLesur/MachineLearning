b3055b102dc3cd747afa66d1a3c0c28898993ce0
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 08:10:17 +0200

    try with local config file

--- onyxia.sh
+++ onyxia.sh
@@ -5,4 +5,4 @@ git clone https://github.com/RLesur/MachineLearning.git --branch onyxia /home/jo
 # Install dependencies
 pip install -r /home/jovyan/work/requirements.txt
 
-echo -e "\n\nc.Spawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /etc/jupyter/jupyter_notebook_config.py
+echo -e "\n\nc.Spawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
