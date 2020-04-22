df49f1b5f7e94a4e9ae7db8ad76573646aa2c786
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 07:23:19 +0200

    change default utl

--- onyxia.sh
+++ onyxia.sh
@@ -4,3 +4,5 @@ git clone https://github.com/RLesur/MachineLearning.git --branch onyxia /home/jo
 
 # Install dependencies
 pip install -r /home/jovyan/work/requirements.txt
+
+jupyterhub --Spawner.default_url='/tree/work/matrice_de_confusion_ROC_AUC.ipynb'
