3ffea135646846bfcd39e83521ce786063533fba
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 06:42:00 +0200

    simplify install step

--- onyxia.sh
+++ onyxia.sh
@@ -1,6 +1,5 @@
 # Clone the repository
 git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
-# Change directory
-cd MachineLearning
-# Install dependencies (from https://stackoverflow.com/a/38609653/6500804)
-#while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
+
+# Install dependencies
+# pip install -r MachineLearning/requirements.txt
