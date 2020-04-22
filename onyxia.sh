caff6de5bd98880adb75dd060959f6f2999269fa
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 06:35:58 +0200

    use a pip requirements.txt file and just install from conda or pip

--- onyxia.sh
+++ onyxia.sh
@@ -1,5 +1,6 @@
-conda init --all
+# Clone the repository
 git clone https://github.com/RLesur/MachineLearning.git --branch onyxia
+# Change directory
 cd MachineLearning
-conda env create -f environment.yml
-bash -c "conda activate ml-environment"
+# Install dependencies (from https://stackoverflow.com/a/38609653/6500804)
+while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
