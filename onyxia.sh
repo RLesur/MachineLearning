7c0571768a09ffa37f1cc796d9d8d3014c201ae6
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 09:19:46 +0200

    rm useless commands

--- onyxia.sh
+++ onyxia.sh
@@ -5,8 +5,8 @@ git clone https://github.com/RLesur/MachineLearning.git --branch onyxia /home/jo
 # Install dependencies
 pip install -r /home/jovyan/work/requirements.txt
 
-echo "\n\nc.DockerSpawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
-echo "\n\nc.Spawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
-echo "\nc.DockerSpawner.remove_containers = True" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+#echo "\n\nc.DockerSpawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+#echo "\n\nc.Spawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+#echo "\nc.DockerSpawner.remove_containers = True" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
 echo "\nc.NotebookApp.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
 #base_url
