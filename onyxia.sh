0163fc0fc4f78486bb0a8971b06f0e487876ee19
Romain Lesur <romain.lesur@gmail.com>
2020-04-22 09:03:54 +0200

    try with NotebookApp

--- onyxia.sh
+++ onyxia.sh
@@ -8,3 +8,4 @@ pip install -r /home/jovyan/work/requirements.txt
 echo -e "\n\nc.DockerSpawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
 echo -e "\n\nc.Spawner.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
 echo -e "\nc.DockerSpawner.remove_containers = True" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+echo -e "\nc.NotebookApp.default_url = '/tree/work/matrice_de_confusion_ROC_AUC.ipynb'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
